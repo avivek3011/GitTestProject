79d5a66dbe49c713c01304e12256b922a99a1596
vivekm <mauryav87@gmail.com>
2021-07-16 18:58:29 +0530

    First commit

---
+++ GitTestProject/src/com/neosoft/GithubTest/GitTest.java
@@ -0,0 +1,15 @@
+package com.neosoft.GithubTest;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		String code = "SE";
+		if (code=="SE") {
+			System.out.println("SE");
+		}
+		else {
+			System.out.println("not a SE");
+		}
+	}
+
+}
